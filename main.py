a3a7fe98ec9f921e985979890635bb83cbd60d8a
Gilberto <gilbertrecupito@gmail.com>
2023-04-17 11:21:41 +0200

    added function to automate repo_analysis of multiple projects

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ import pandas as pd
 from get_list_file_py import find_python_files, get_python_files
 
 
-def analyze_project(project_path):
+def analyze_project(project_path, output_path="."):
     col = ["filename", "function_name", "smell", "name_smell", "message"]
     to_save = pd.DataFrame(columns=col)
-    project_name = ""
-    filenames =get_python_files(project_path)
+    filenames = get_python_files(project_path)
 
     for filename in filenames:
         file_path = os.path.join(filename)
         with open(file_path, "rb") as file:
-                source = file.read()
+            source = file.read()
+            try:
                 tree = ast.parse(source)
                 libraries = extract_libraries(tree)
                 # Visita i nodi dell'albero dell'AST alla ricerca di funzioni
@@ -64,11 +64,33 @@ def analyze_project(project_path):
                             to_save.loc[len(to_save)] = tensor
                         if pytorch:
                             to_save.loc[len(to_save)] = pytorch
+            except Exception as e:
+                message = f"Error in file {filename}: {e}"
+                error_path = output_path.replace("output", "error")
+                if not os.path.exists(error_path):
+                    os.makedirs(error_path)
+                with open(f"{error_path}/error.txt", "a") as error_file:
+                    error_file.write(message)
+                continue
 
-       # for dirname in dirnames:
-       #     new_path = os.path.join(dirpath, dirname)
-       #     analyze_project(new_path)
-    to_save.to_csv("output/to_save.csv", index=False, mode='a')
+    # for dirname in dirnames:
+    #     new_path = os.path.join(dirpath, dirname)
+    #     analyze_project(new_path)
+    to_save.to_csv(output_path + "/to_save.csv", index=False, mode='a')
 
 
-analyze_project("/Users/broke31/Desktop/smell_ai")
+# analyze_project("/Users/broke31/Desktop/smell_ai")
+
+def projects_analysis(BASE_PATH, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    dirpath = os.listdir(BASE_PATH)
+    for dirname in dirpath:
+        new_path = os.path.join(BASE_PATH, dirname)
+        if not os.path.exists(f"{output_path}/{dirname}"):
+            os.makedirs(f"{output_path}/{dirname}")
+        analyze_project(new_path, f"{output_path}/{dirname}")
+        print(f"Analyzed {dirname}")
+
+if __name__ == "__main__":
+    projects_analysis("./dataset/projects", "./projects_analysis/output")
