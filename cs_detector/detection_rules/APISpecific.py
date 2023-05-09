84bde86cb331a68ee8b18739a1d307be96f96dd1
gilbertrec <43886382+gilbertrec@users.noreply.github.com>
2023-05-09 16:20:25 +0200

    refactored matrix multiplication

--- cs_detector/detection_rules/APISpecific.py
+++ cs_detector/detection_rules/APISpecific.py
@@ -2,8 +2,11 @@ import ast
 import re
 
 from cs_detector.code_extractor.dataframe_detector import dataframe_check
+from cs_detector.code_extractor.variables import search_variable_definition
+from cs_detector.code_extractor.libraries import extract_library_as_name
 
 test_libraries = ["pytest", "robot", "unittest", "doctest", "nose2", "testify", "pytest-cov", "pytest-xdist"]
+
 
 def Chain_Indexing(libraries, filename, node):
     if [x for x in libraries if x in test_libraries]:
@@ -22,11 +25,10 @@ def Chain_Indexing(libraries, filename, node):
     return []
 
 
-def dataframe_conversion_api_misused(libraries, filename, fun_node,df_dict):
-
+def dataframe_conversion_api_misused(libraries, filename, fun_node, df_dict):
     if [x for x in libraries if 'pandas' in x]:
         function_name = fun_node.name
-        variables = dataframe_check(fun_node,libraries,df_dict)
+        variables = dataframe_check(fun_node, libraries, df_dict)
     number_of_apply = 0
     for node in ast.walk(fun_node):
         if isinstance(node, ast.Attribute):
@@ -36,7 +38,6 @@ def dataframe_conversion_api_misused(libraries, filename, fun_node,df_dict):
                         if hasattr(node, 'value'):
                             if node.value.id in variables:
                                 number_of_apply += 1
-                                print("node.id", node.value.id)
     if number_of_apply > 0:
         message = "Please consider to use numpy instead values to convert dataframe. The function 'values' is deprecated." \
                   "The value return of this function is unclear."
@@ -46,18 +47,63 @@ def dataframe_conversion_api_misused(libraries, filename, fun_node,df_dict):
     return []
 
 
-def matrix_multiplication_api_misused(libraries, filename, node):
+def matrix_multiplication_api_misused(libraries, filename, fun_node):
+    number_of_apply = 0
+    library_name = ""
     if [x for x in libraries if x in test_libraries]:
         return []
     if [x for x in libraries if 'numpy' in x]:
-        function_name = node.name
-        function_body = ast.unparse(node.body).strip()
-        number_of_dot = function_body.count(".dot(")
-        message = "Please consider to use np.matmul to multiply matrix. The function dot() not return a scalar value, " \
-                  "but a matrix. "
-        if number_of_dot > 0:
+        for x in libraries:
+            if 'numpy' in x:
+                library_name = extract_library_as_name(x)
+                function_name = fun_node.name
+        if library_name == "":
+            return []
+        for node in ast.walk(fun_node):
+            # search for dot function usages
+            if isinstance(node, ast.Call):
+                if hasattr(node, 'func'):
+                    if hasattr(node.func, 'attr'):
+                        if node.func.attr == 'dot' and node.func.value.id == library_name:
+                            # if dot function used with constant matrices, increase number of apply
+                            if hasattr(node, 'args'):
+                                if len(node.args) > 1:
+                                    arguments = []
+                                    matrix_multiplication = False
+                                    for arg in node.args:
+                                        # check if each argument is a list
+                                        if isinstance(arg, ast.List):
+
+                                            # check if each list contains a list, so it is a matrix
+                                            for el in arg.elts:
+
+                                                if isinstance(el, ast.List):
+                                                    matrix_multiplication = True
+
+                                        else:
+                                            if isinstance(arg, ast.Name):
+                                                # in this case we have to extract variables and see if it is a matrix
+                                                arguments.append(arg.id)
+                                    if matrix_multiplication:
+                                        number_of_apply += 1
+                                        print("Matrices are node.args: ", ast.unparse(node))
+                                    else:
+                                        for arg in arguments:
+                                            node_def = search_variable_definition(arg, fun_node, node)
+                                            if node_def is not None:
+                                                constant = node_def.value
+                                                if isinstance(constant, ast.List):
+                                                    for el in constant.elts:
+                                                        if isinstance(el, ast.List):
+                                                            matrix_multiplication = True
+                                        if matrix_multiplication:
+                                            number_of_apply += 1
+                                            print("Matrices are node.args: ", ast.unparse(node))
+        if number_of_apply > 0:
+            message = "Please consider to use np.matmul to multiply matrix. The function dot() not return a scalar value, " \
+                      "but a matrix."
             name_smell = "matrix_multiplication_api_misused"
-            to_return = [filename, function_name, number_of_dot, name_smell, message]
+            to_return = [filename, function_name, number_of_apply, name_smell, message]
             return to_return
         return []
     return []
@@ -96,7 +142,6 @@ def gradients_not_cleared_before_backward_propagation(libraries, filename, node)
             return to_return
         return []
     return []
-
 
 
 def tensor_array_not_used(libraries, filename, fun_node):
@@ -138,7 +183,7 @@ def pytorch_call_method_misused(libraries, filename, node):
                 number_of_forward += 1
         if number_of_forward > 0:
             message = "is recommended to use self.net()"
-            name_smell ="pytorch_call_method_misused"
+            name_smell = "pytorch_call_method_misused"
             to_return = [filename, function_name, number_of_forward, name_smell, message]
             return to_return
         return []
