4b1be090486054c1700aaf4e7bec01e9707ae859
XJustUnluckyX <xzenitz@gmail.com>
2024-12-01 20:08:18 +0100

    added incident testing branch and TC2 folder; modified gitignore

---
+++ test/TC2/MockDirectory2/dataframe_conversion_misused.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+def dataframe_conversion_api_misused():
+    data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+    df = pd.DataFrame(data)
+
+    # Uso scorretto di `.values`
+    array = df.values  # Punto di rilevamento
