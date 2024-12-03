f621b545188b57cbb069e2a413fb4660de643ef6
xDaryamo <mr.dario.mazza@gmail.com>
2024-12-03 15:02:00 +0100

    Add TC3, TC11, TC12, TC13, TC17

---
+++ test/TC12/MockDirectory/columns_dtype_not_set.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def columns_and_datatype_not_explicitly_set_example():
+    df = pd.read_csv('data.csv')
+
+
+def load_data(file_path):
+    df = pd.read_csv(file_path)  # Manca esplicitazione di `dtype`
+    return df
